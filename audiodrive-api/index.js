33bfe61aec4a0e1ea970c9c220cd6d51d0ff6dfc
Khalid Talakshi <khalid.talakshi@outlook.com>
2021-12-28 00:22:50 -0500

    added graphql to server

--- audiodrive-api/index.js
+++ audiodrive-api/index.js
@@ -1,6 +1,30 @@
 import express from "express";
+import { ApolloServer } from "apollo-server-express"
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+import cors from "cors"
 
 const app = express()
+
+const apolloServer = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+    cors: false,
+})
+
+await apolloServer.start();
+
+var corsOptions = {
+    origin: "*",
+    credentials: false, // <-- REQUIRED backend setting
+};
+
+apolloServer.applyMiddleware({
+    app,
+    path: "/graphql",
+});
+
+app.use(cors(corsOptions));
 
 app.listen(4000, () => {
     console.log("server is running")
