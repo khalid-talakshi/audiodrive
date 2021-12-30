e324684ea3d9043d190a673d28ea9dad1e16212b
Khalid Talakshi <khalid.talakshi@outlook.com>
2021-12-30 02:27:56 -0500

    added @apollo/client to mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,18 +1,30 @@
 import { NavigationContainer, DarkTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {registerRootComponent} from 'expo'
+import { registerRootComponent } from "expo";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Home } from "./pages";
+
+const client = new ApolloClient({
+  uri: "localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer theme={DarkTheme}>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{title: "AudioDrive"}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: "AudioDrive" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
 
-export default registerRootComponent(App)
+export default registerRootComponent(App);
